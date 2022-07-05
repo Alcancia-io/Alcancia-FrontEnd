6bf8d0af8893bc0e682d817c957a073b9361f47f
Santiago Quihui <santiagoquihui@hotmail.com>
2022-07-05 16:28:40 -0700

    Create AlcanciaTheme for light and dark mode

---
+++ lib/themes/app_theme.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AlcanciaTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      brightness: Brightness.light,
+      scaffoldBackgroundColor: alcanciaBgLight,
+      primaryColor: alcanciaBgLight,
+      cardColor: alcanciaBgLight,
+      backgroundColor: alcanciaBgLight,
+      fontFamily: 'Gotham',
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          textStyle: MaterialStateProperty.all(const TextStyle(fontFamily: 'Gotham')),
+        ),
+      ),
+      cupertinoOverrideTheme: const NoDefaultCupertinoThemeData(
+        textTheme: CupertinoTextThemeData(
+          textStyle: TextStyle(fontFamily: 'Gotham'),
+        ),
+      ),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: alcanciaBgDark,
+      primaryColor: alcanciaBgDark,
+      cardColor: alcanciaBgDark,
+      backgroundColor: alcanciaBgDark,
+      fontFamily: "Gotham",
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          textStyle: MaterialStateProperty.all(const TextStyle(fontFamily: 'Gotham')),
+        ),
+      ),
+      cupertinoOverrideTheme: const NoDefaultCupertinoThemeData(
+        textTheme: CupertinoTextThemeData(
+          textStyle: TextStyle(fontFamily: 'Gotham'),
+        ),
+      ),
+    );
+  }
+}
