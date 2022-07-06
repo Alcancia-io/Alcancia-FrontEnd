3b2982b9cda6fe0fa0866486e36d478e41a39a61
manu-yaff <manuelyg2097@gmail.com>
2022-07-06 18:24:30 -0500

    Add login screen progress

--- lib/src/features/login/presentation/login_screen.dart
+++ lib/src/features/login/presentation/login_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: Text("Login")),
+    final size = MediaQuery.of(context).size;
+    var isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Center(
+              child: SvgPicture.asset(
+                isDarkMode
+                    ? "lib/src/resources/images/icon_alcancia_dark_no_letters.svg"
+                    : "lib/src/resources/images/icon_alcancia_light_no_letters.svg",
+                height: size.height / 8,
+              ),
+            ),
+            Row(children: const [
+              Text(
+                '!Hola!\nBienvenido',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+              ),
+            ]),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/src/features/welcome/presentation/welcome_screen.dart
+++ lib/src/features/welcome/presentation/welcome_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:alcancia/src/resources/colors.dart';
+import 'package:alcancia/src/resources/colors/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
