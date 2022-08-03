3ab42cd1b61a74b050abea5492960b3d411044eb
Santiago Quihui <santiagoquihui@hotmail.com>
2022-08-02 21:49:59 -0500

    Add text styles to app theme

--- lib/src/resources/colors/app_theme.dart
+++ lib/src/resources/colors/app_theme.dart
@@ -3,27 +3,44 @@ import 'package:flutter/material.dart';
 import 'colors.dart';
 
 class AlcanciaTheme {
+
+  static const TextTheme _textTheme = TextTheme(
+      headline1: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+      headline2: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+      subtitle1: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      subtitle2: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
+      bodyText1: TextStyle(fontWeight: FontWeight.w400, fontSize: 15),
+      bodyText2: TextStyle(fontWeight: FontWeight.w400, fontSize: 13),
+      caption: TextStyle(fontWeight: FontWeight.w400, fontSize: 10),
+      button: TextStyle(fontWeight: FontWeight.w400, fontSize: 18)
+  );
+
   static ThemeData get lightTheme {
     return ThemeData(
+      canvasColor: alcanciaBgLight,
       brightness: Brightness.light,
       scaffoldBackgroundColor: alcanciaBgLight,
       primaryColor: alcanciaBgLight,
       cardColor: alcanciaBgLight,
       backgroundColor: alcanciaBgLight,
       fontFamily: 'Gotham',
+      textTheme: _textTheme,
       textButtonTheme: TextButtonThemeData(
         style: ButtonStyle(
           textStyle:
               MaterialStateProperty.all(const TextStyle(fontFamily: 'Gotham')),
         ),
       ),
+      iconTheme: const IconThemeData(
+          color: Colors.black
+      ),
       inputDecorationTheme: InputDecorationTheme(
         border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(7),
             borderSide: BorderSide.none),
         errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(7),
-            borderSide: const BorderSide(color: Colors.red),
+          borderRadius: BorderRadius.circular(7),
+          borderSide: const BorderSide(color: Colors.red),
         ),
         filled: true,
         fillColor: alcanciaFieldLight,
@@ -45,17 +62,22 @@ class AlcanciaTheme {
 
   static ThemeData get darkTheme {
     return ThemeData(
+      canvasColor: alcanciaBgDark,
       brightness: Brightness.dark,
       scaffoldBackgroundColor: alcanciaBgDark,
       primaryColor: alcanciaBgDark,
       cardColor: alcanciaBgDark,
       backgroundColor: alcanciaBgDark,
       fontFamily: "Gotham",
+      textTheme: _textTheme,
       textButtonTheme: TextButtonThemeData(
         style: ButtonStyle(
           textStyle:
               MaterialStateProperty.all(const TextStyle(fontFamily: 'Gotham')),
         ),
+      ),
+      iconTheme: const IconThemeData(
+          color: Colors.white,
       ),
       inputDecorationTheme: InputDecorationTheme(
         border: OutlineInputBorder(
