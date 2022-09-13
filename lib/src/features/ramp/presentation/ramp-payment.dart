961253eb83ee6c4eda372bf34a131051efb0c812
Juan David Torres Velasco <juandatorr_1999@hotmail.com>
2022-09-13 13:25:50 -0500

    chore: ramp demo

---
+++ lib/src/features/ramp/presentation/ramp-payment.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:ramp_flutter/ramp_flutter.dart';
+
+class RampPayment extends StatelessWidget {
+  const RampPayment({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: _presentRamp,
+        child: const Text("Present Ramp"),
+      ),
+    );
+  }
+
+  void _presentRamp() {
+    Configuration configuration = Configuration();
+    configuration.fiatCurrency = "EUR";
+    configuration.fiatValue = "15";
+    configuration.deepLinkScheme = "myawesomeapp";
+    RampFlutter.showRamp(
+        configuration, onPurchaseCreated, onRampFailed, onRampClosed);
+  }
+
+  void onPurchaseCreated(Purchase purchase, String token, String url) {}
+
+  void onRampFailed() {}
+
+  void onRampClosed() {}
+}
