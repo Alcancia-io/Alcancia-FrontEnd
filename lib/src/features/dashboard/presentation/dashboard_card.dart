349e34e08a293e2bdb654dd3592677542c6da64e
Juan David Torres Velasco <juandatorr_1999@hotmail.com>
2022-09-28 21:09:35 -0500

    fix: dashboard card update

--- lib/src/features/dashboard/presentation/dashboard_card.dart
+++ lib/src/features/dashboard/presentation/dashboard_card.dart
@@ -66,37 +66,20 @@ class DashboardCard extends StatelessWidget {
                     children: [
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.only(
-                              top: 6,
-                              bottom: 6,
-                              left: 12,
+                        children: const [
+                          Text(
+                            "Balance",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
                             ),
-                            decoration: BoxDecoration(
-                              color: ctx.brightness == Brightness.dark
-                                  ? Color(0xFF1F318C)
-                                  : Color.fromARGB(255, 217, 217, 217),
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(8),
-                              ),
-                            ),
-                            width: 198,
-                            height: 30,
-                            child: const Text(
-                              'Balance',
-                              style: TextStyle(
-                                // color: Colors.white,
-                                fontSize: 15,
-                              ),
-                            ),
-                          )
+                          ),
                         ],
                       ),
                       Container(
                         padding: const EdgeInsets.only(top: 16, bottom: 24),
                         child: Text(
-                          "\$ ${balance} USDC",
+                          "\$${balance.toStringAsFixed(3)} USDC",
                           style: const TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 35,
@@ -109,12 +92,11 @@ class DashboardCard extends StatelessWidget {
                         ),
                         builder: (QueryResult result,
                             {VoidCallback? refetch, FetchMore? fetchMore}) {
-                          // Commented for now to show the buttons
                           // if (result.hasException) {
                           //   return Text("error");
                           // }
                           if (result.isLoading) {
-                            return CircularProgressIndicator();
+                            return Text("is loading...");
                           }
                           var userProfit = result.data?['getUserProfit'];
 
