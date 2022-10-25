fd44eb530507d1b34f37d49618243c2b5fcda7dc
manu-yaff <manuelyg2097@gmail.com>
2022-10-25 13:25:33 -0500

    refactor: add transactions query to queries folder

---
+++ lib/src/shared/graphql/queries/transactions_query.dart
@@ -0,0 +1,16 @@
+const String transactionsQuery = """
+  query(\$userTransactionsInput: UserTransactionsInput!){
+    getUserTransactions(getUserTransactionsInput: \$userTransactionsInput) {
+      totalItems,
+      items {
+        createdAt,
+        id,
+        sourceAmount,
+        sourceAsset,
+        targetAsset,
+        amount,
+        type
+      }
+    }
+  }
+""";
