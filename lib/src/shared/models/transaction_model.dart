c9b0918f3955d7392a2eb446c5c890dfaaa8e23e
manu-yaff <manuelyg2097@gmail.com>
2022-10-25 13:26:48 -0500

    refactor: add transaction model

---
+++ lib/src/shared/models/transaction_model.dart
@@ -0,0 +1,31 @@
+class Transaction {
+  String transactionID;
+  String createdAt;
+  int sourceAmount;
+  String sourceAsset;
+  String targetAsset;
+  int amount;
+  String type;
+
+  Transaction({
+    required this.transactionID,
+    required this.createdAt,
+    required this.sourceAmount,
+    required this.sourceAsset,
+    required this.targetAsset,
+    required this.amount,
+    required this.type,
+  });
+
+  factory Transaction.fromJson(Map<String, dynamic> json) {
+    return Transaction(
+      transactionID: json["id"] as String,
+      createdAt: json["createdAt"] as String,
+      sourceAmount: json["sourceAmount"] as int,
+      sourceAsset: json["sourceAsset"] as String,
+      targetAsset: json["targetAsset"] as String,
+      amount: json["amount"] as int,
+      type: json["type"] as String,
+    );
+  }
+}
