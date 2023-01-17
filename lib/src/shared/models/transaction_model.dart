773a8c161cf4d2ee7c3b0767d0b7e0c0ca5da129
Santiago Quihui <santiagoquihui@hotmail.com>
2023-01-17 15:12:57 -0700

    add status to transaction model

--- lib/src/shared/models/transaction_model.dart
+++ lib/src/shared/models/transaction_model.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Transaction {
   String transactionID;
   String createdAt;
@@ -6,6 +8,7 @@ class Transaction {
   String targetAsset;
   int amount;
   String type;
+  String status;
 
   Transaction({
     required this.transactionID,
@@ -15,17 +18,30 @@ class Transaction {
     required this.targetAsset,
     required this.amount,
     required this.type,
+    required this.status,
   });
 
   factory Transaction.fromJson(Map<String, dynamic> json) {
+    var targetAsset = json["targetAsset"] as String;
+    if (targetAsset == "aPolUSDC") targetAsset = "USDC";
     return Transaction(
       transactionID: json["id"] as String,
       createdAt: json["createdAt"] as String,
       sourceAmount: json["sourceAmount"] as int,
       sourceAsset: json["sourceAsset"] as String,
-      targetAsset: json["targetAsset"] as String,
+      targetAsset: targetAsset,
       amount: json["amount"] as int,
       type: json["type"] as String,
+      status: json["status"] as String
     );
+  }
+}
+
+
+extension StatusIcon on Transaction {
+  Icon get iconForTxnStatus {
+    if (status == "PENDING") return const Icon(Icons.hourglass_bottom_rounded);
+    if (status == "COMPLETED") return const Icon(Icons.check, color: Colors.green,);
+    return const Icon(Icons.close, color: Colors.red,);
   }
 }
