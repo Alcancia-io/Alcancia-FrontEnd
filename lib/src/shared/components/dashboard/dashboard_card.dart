8fce72d6cca72af74253cece937148ebbe01d2ed
Santiago Quihui <santiagoquihui@hotmail.com>
2023-01-26 12:52:23 -0700

    feat: carousel slider in dashboard

--- lib/src/shared/components/dashboard/dashboard_card.dart
+++ lib/src/shared/components/dashboard/dashboard_card.dart
@@ -4,6 +4,15 @@ import 'package:alcancia/src/shared/provider/balance_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class BalanceItem {
+  BalanceItem({required this.title, required this.value, required this.currency});
+
+  String title;
+  double value;
+  String currency;
+}
 
 class DashboardCard extends ConsumerWidget {
   DashboardCard({Key? key}) : super(key: key);
@@ -13,6 +22,12 @@ class DashboardCard extends ConsumerWidget {
     final ctx = Theme.of(context);
     final userBalance = ref.watch(balanceProvider);
 
+    final totalBalanceItem = BalanceItem(title: "Balance Total", value: userBalance.total, currency: "USD");
+    final cUSDBalanceItem =
+        BalanceItem(title: "Balance Celo USD", value: userBalance.mcUSD + userBalance.cUSD, currency: "CUSD");
+    final usdcBalanceItem =
+        BalanceItem(title: "Balance USD Coin", value: userBalance.etherscan + userBalance.aPolUSDC, currency: "USDC");
+    final carouselItems = [totalBalanceItem, usdcBalanceItem, cUSDBalanceItem];
     return Container(
       padding: const EdgeInsets.all(16.0),
       decoration: BoxDecoration(
@@ -24,27 +39,37 @@ class DashboardCard extends ConsumerWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
-              Text(
-                "Balance",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+          CarouselSlider.builder(
+            itemCount: carouselItems.length,
+            itemBuilder: (BuildContext context, int itemIndex, int _) {
+              final balance = carouselItems[itemIndex];
+              return SizedBox(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      balance.title,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 16, bottom: 24),
+                      child: Text(
+                        "\$${balance.value} ${balance.currency}",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.only(top: 16, bottom: 24),
-            child: Text(
-              "\$${userBalance.total} USDC",
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
-            ),
+              );
+            },
+            options: CarouselOptions(viewportFraction: 1, height: 100),
           ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
