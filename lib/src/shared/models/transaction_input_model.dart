94bf9c07d490e1274795b1cde19a86cdbc9f63cd
manu-yaff <manuelyg2097@gmail.com>
2023-01-26 21:15:20 -0600

    feat: update transaction input to receive targetCurrency

--- lib/src/shared/models/transaction_input_model.dart
+++ lib/src/shared/models/transaction_input_model.dart
@@ -3,17 +3,19 @@ enum TransactionType { deposit, withdrawal }
 enum TransactionMethod { cryptopay, suarmi }
 
 class TransactionInput {
-  TransactionInput({
-    required this.txnMethod,
-    required this.txnType,
-    required this.sourceAmount,
-    required this.targetAmount,
-    this.concept,
-  });
-
   final TransactionMethod txnMethod;
   final TransactionType txnType;
   final double sourceAmount;
   final double targetAmount;
   final String? concept;
+  final String targetCurrency;
+
+  TransactionInput({
+    required this.txnMethod,
+    required this.txnType,
+    required this.sourceAmount,
+    required this.targetAmount,
+    required this.targetCurrency,
+    this.concept,
+  });
 }
