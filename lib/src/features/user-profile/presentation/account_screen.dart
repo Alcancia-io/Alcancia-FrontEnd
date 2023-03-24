a3c1e8ff9b8cfe3ad960176f44ee7b7a25ee6043
Santiago Quihui <39177932+kiwis08@users.noreply.github.com>
2023-03-24 16:37:11 -0600

    fix: disable change password button (#91)

--- lib/src/features/user-profile/presentation/account_screen.dart
+++ lib/src/features/user-profile/presentation/account_screen.dart
@@ -31,78 +31,80 @@ class AccountScreen extends ConsumerWidget {
           padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 10),
           child: Column(
             children: [
-              GestureDetector(
-                onTap: () {
-                  // TODO: Change password
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).brightness == Brightness.dark
-                        ? alcanciaCardDark2
-                        : alcanciaCardLight2,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Icon(Icons.lock_outlined),
-                        ),
-                        Text(
-                          appLoc.labelChangePassword,
-                          style: Theme.of(context).textTheme.labelLarge,
-                        ),
-                        Spacer(),
-                        Icon(Icons.chevron_right)
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+              // GestureDetector(
+              //   onTap: () {
+              //     // TODO: Change password
+              //   },
+              //   child: Container(
+              //     decoration: BoxDecoration(
+              //       color: Theme.of(context).brightness == Brightness.dark
+              //           ? alcanciaCardDark2
+              //           : alcanciaCardLight2,
+              //       borderRadius: BorderRadius.circular(8),
+              //     ),
+              //     child: Padding(
+              //       padding: const EdgeInsets.all(16.0),
+              //       child: Row(
+              //         children: [
+              //           Padding(
+              //             padding: const EdgeInsets.all(8.0),
+              //             child: Icon(Icons.lock_outlined),
+              //           ),
+              //           Text(
+              //             appLoc.labelChangePassword,
+              //             style: Theme.of(context).textTheme.labelLarge,
+              //           ),
+              //           Spacer(),
+              //           Icon(Icons.chevron_right)
+              //         ],
+              //       ),
+              //     ),
+              //   ),
+              // ),
               Spacer(),
-              Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: AlcanciaButton(
-                    foregroundColor: Colors.red,
-                    side: BorderSide(color: Colors.red),
-                    buttonText: appLoc.buttonDeleteAccount,
-                    fontSize: 18,
-                    padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 4.0, bottom: 4.0),
-                    onPressed: () async {
-                      await showDialog(
-                          context: context,
-                          builder: (BuildContext ctx) {
-                            return AlcanciaActionDialog(
-                              acceptText: appLoc.buttonConfirm,
-                              acceptColor: Colors.red,
-                              cancelText: appLoc.buttonCancel,
-                              acceptAction: () async {
-                                try {
-                                  await authService.deleteAccount();
-                                  await deleteToken();
-                                  context.goNamed("welcome");
-                                  ref.read(userProvider.notifier).setUser(null);
-                                } catch (e) {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                      AlcanciaSnackBar(context,
-                                          appLoc.errorDeleteAccount));
-                                }
-                              },
-                              child: Text(
-                                appLoc.labelDeleteAccountConfirmation,
-                                style: Theme.of(context).textTheme.titleLarge,
-                              ),
-                            );
-                          });
-                    },
-                    rounded: true,
-                    icon: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Icon(Icons.delete_forever),
-                    ),
-                  )),
+              Center(
+                child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: AlcanciaButton(
+                      foregroundColor: Colors.red,
+                      side: BorderSide(color: Colors.red),
+                      buttonText: appLoc.buttonDeleteAccount,
+                      fontSize: 18,
+                      padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 4.0, bottom: 4.0),
+                      onPressed: () async {
+                        await showDialog(
+                            context: context,
+                            builder: (BuildContext ctx) {
+                              return AlcanciaActionDialog(
+                                acceptText: appLoc.buttonConfirm,
+                                acceptColor: Colors.red,
+                                cancelText: appLoc.buttonCancel,
+                                acceptAction: () async {
+                                  try {
+                                    await authService.deleteAccount();
+                                    await deleteToken();
+                                    context.goNamed("welcome");
+                                    ref.read(userProvider.notifier).setUser(null);
+                                  } catch (e) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                        AlcanciaSnackBar(context,
+                                            appLoc.errorDeleteAccount));
+                                  }
+                                },
+                                child: Text(
+                                  appLoc.labelDeleteAccountConfirmation,
+                                  style: Theme.of(context).textTheme.titleLarge,
+                                ),
+                              );
+                            });
+                      },
+                      rounded: true,
+                      icon: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(Icons.delete_forever),
+                      ),
+                    )),
+              ),
             ],
           ),
         ),
