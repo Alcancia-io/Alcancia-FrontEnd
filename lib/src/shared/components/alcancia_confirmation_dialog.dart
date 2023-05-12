21aa34ccf3a969b9ad9ccd9c5cf71e65810badd8
Santiago Quihui <santiagoquihui@hotmail.com>
2023-05-12 11:08:41 -0600

    remove mock data from confirmation dialog

--- lib/src/shared/components/alcancia_confirmation_dialog.dart
+++ lib/src/shared/components/alcancia_confirmation_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:alcancia/src/shared/components/alcancia_components.dart';
+import 'package:alcancia/src/shared/models/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
@@ -6,95 +7,92 @@ import 'package:go_router/go_router.dart';
 import '../../resources/colors/colors.dart';
 
 class AlcanciaConfirmationDialog extends StatelessWidget {
-  const AlcanciaConfirmationDialog({super.key});
+  const AlcanciaConfirmationDialog({super.key, required this.targetUser, required this.userBalance, required this.amount});
 
-  final balanceAmount = 'Balance - 20.00 USDC';
-  final phone = '+52 465 107 5213';
-  final name = 'David Lerma';
-  final amount = '15 usdc';
+  final User targetUser;
+  final double userBalance;
+  final double amount;
 
   @override
   Widget build(BuildContext context) {
     final appLoc = AppLocalizations.of(context)!;
     final txtTheme = Theme.of(context).textTheme;
 
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: Theme.of(context).brightness == Brightness.dark
-                ? alcanciaFieldDark
-                : alcanciaFieldLight,
-          ),
-          margin: const EdgeInsets.symmetric(horizontal: 30),
-          height: 437,
-          width: double.infinity,
-          padding: const EdgeInsets.only(
-            top: 44,
-            right: 36,
-            bottom: 30,
-            left: 30,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(bottom: 36),
-                child: Text(
-                  appLoc.isDataCorrect,
-                  style: txtTheme.subtitle1,
-                ),
+    return Dialog(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Theme.of(context).brightness == Brightness.dark
+              ? alcanciaFieldDark
+              : alcanciaFieldLight,
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 30),
+        height: 437,
+        width: double.infinity,
+        padding: const EdgeInsets.only(
+          top: 44,
+          right: 36,
+          bottom: 30,
+          left: 30,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 36),
+              child: Text(
+                appLoc.isDataCorrect,
+                style: txtTheme.subtitle1,
               ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 44),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 16),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(appLoc.sourceAccount, style: txtTheme.subtitle2),
-                          Text(balanceAmount, style: txtTheme.subtitle2),
-                        ],
-                      ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 44),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(appLoc.sourceAccount, style: txtTheme.subtitle2),
+                        Text('Balance - ${userBalance.toStringAsFixed(2)}', style: txtTheme.subtitle2),
+                      ],
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 16),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(appLoc.targetPhone, style: txtTheme.subtitle2),
-                          Text(phone, style: txtTheme.subtitle2),
-                          Text(name, style: txtTheme.subtitle2),
-                        ],
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(appLoc.targetPhone, style: txtTheme.subtitle2),
+                        Text(targetUser.phoneNumber, style: txtTheme.subtitle2),
+                        Text(targetUser.name, style: txtTheme.subtitle2),
+                      ],
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(appLoc.amount, style: txtTheme.subtitle2),
-                          Text(amount, style: txtTheme.subtitle2),
-                        ],
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(appLoc.amount, style: txtTheme.subtitle2),
+                        Text(amount.toString(), style: txtTheme.subtitle2),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              AlcanciaButton(
+            ),
+            AlcanciaButton(
                 width: double.infinity,
                 height: 64,
                 buttonText: 'Si transferir',
                 onPressed: (){
                   context.pushNamed('successful-transaction');
                 }
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
