f6d61babcb0f0bcf78de98c51ecd4f704bb610c8
Santiago Quihui <39177932+kiwis08@users.noreply.github.com>
2023-07-05 11:14:48 -0600

    chore: add "coming soon" to crypto deposits (#130)
    * chore: add "coming soon" to crypto deposits
    
    * fix: disable deposit option onTap in comingSoon mode

--- lib/src/screens/deposit/deposit_option.dart
+++ lib/src/screens/deposit/deposit_option.dart
@@ -1,6 +1,7 @@
 import 'package:alcancia/src/resources/colors/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class DepositOption extends StatelessWidget {
   const DepositOption({
@@ -11,6 +12,7 @@ class DepositOption extends StatelessWidget {
     required this.pill1,
     required this.pill2,
     required this.onTap,
+    this.comingSoon = false,
   }) : super(key: key);
 
   final String imageSrc;
@@ -18,98 +20,128 @@ class DepositOption extends StatelessWidget {
   final String subtitle;
   final String pill1;
   final String pill2;
-  final void Function() onTap;
+  final void Function()? onTap;
+  final bool comingSoon;
 
   @override
   Widget build(BuildContext context) {
     final darkMode = Theme.of(context).brightness == Brightness.dark;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: InkWell(
-        splashFactory: InkSplash.splashFactory,
-        splashColor: darkMode ? Colors.grey : Colors.red,
-        borderRadius: BorderRadius.circular(10),
-        onTap: onTap,
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Theme.of(context).brightness == Brightness.dark
-                ? alcanciaCardDark2
-                : alcanciaCardLight2,
-          ),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SvgPicture.asset(
-                  imageSrc,
-                  height: 40,
+    final comingSoonColor = darkMode
+        ? alcanciaMidBlue
+        : alcanciaLightBlue;
+    final appLoc = AppLocalizations.of(context)!;
+    return Opacity(
+      opacity: comingSoon ? 0.5 : 1,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: InkWell(
+          splashFactory: InkSplash.splashFactory,
+          splashColor: darkMode ? Colors.grey : Colors.red,
+          borderRadius: BorderRadius.circular(10),
+          onTap: comingSoon ? null : onTap,
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Theme.of(context).brightness == Brightness.dark
+                  ? alcanciaCardDark2
+                  : alcanciaCardLight2,
+            ),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SvgPicture.asset(
+                    imageSrc,
+                    height: 40,
+                  ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Text(subtitle),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(subtitle),
+                    ),
+                    if (comingSoon) ... [
                       Padding(
                         padding: const EdgeInsets.only(right: 8.0),
                         child: Container(
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(20),
-                            color: darkMode
-                                ? Color(0xFF9AFF47).withOpacity(0.2)
-                                : Color(0xFF31DE52).withOpacity(0.2),
+                            color: comingSoonColor,
                           ),
                           child: Padding(
                             padding: const EdgeInsets.all(5.0),
                             child: Text(
-                              pill1,
+                              appLoc.labelComingSoon,
                               style: TextStyle(
-                                  color: darkMode
-                                      ? Color(0xFF31DE52)
-                                      : Colors.green),
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold
+                              ),
                             ),
                           ),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: darkMode
-                                ? Color(0xFF9AFF47).withOpacity(0.2)
-                                : Color(0xFF31DE52).withOpacity(0.2),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Text(
-                              pill2,
-                              style: TextStyle(
-                                  color: darkMode
-                                      ? Color(0xFF31DE52)
-                                      : Colors.green),
+                    ] else ... [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: darkMode
+                                    ? Color(0xFF9AFF47).withOpacity(0.2)
+                                    : Color(0xFF31DE52).withOpacity(0.2),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text(
+                                  pill1,
+                                  style: TextStyle(
+                                      color: darkMode
+                                          ? Color(0xFF31DE52)
+                                          : Colors.green),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: darkMode
+                                    ? Color(0xFF9AFF47).withOpacity(0.2)
+                                    : Color(0xFF31DE52).withOpacity(0.2),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text(
+                                  pill2,
+                                  style: TextStyle(
+                                      color: darkMode
+                                          ? Color(0xFF31DE52)
+                                          : Colors.green),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  )
-                ],
-              ),
-              Spacer(),
-              Icon(Icons.chevron_right_outlined),
-            ],
+                    ]
+                  ],
+                ),
+                Spacer(),
+                Icon(Icons.chevron_right_outlined),
+              ],
+            ),
           ),
         ),
       ),
