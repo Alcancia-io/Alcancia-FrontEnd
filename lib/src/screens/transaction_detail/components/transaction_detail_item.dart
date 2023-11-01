200b5049226c49ad376ac543e56850494b8a7252
AlbertoMorillo <amorilloavila25@hotmail.com>
2023-11-01 19:55:24 -0400

    Copy Clipboard transaction ID (#185)

--- lib/src/screens/transaction_detail/components/transaction_detail_item.dart
+++ lib/src/screens/transaction_detail/components/transaction_detail_item.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../../../shared/components/alcancia_copy_clipboard.dart';
 
 class TransactionDetailItem extends StatelessWidget {
   final String leftText;
   final String? rightText;
   final Widget? rightIcon;
-
-  const TransactionDetailItem(
-      {Key? key, required this.leftText, this.rightText, this.rightIcon})
-      : super(key: key);
+  final bool supportsClipboard;
+  const TransactionDetailItem({
+    Key? key,
+    required this.leftText,
+    this.rightText,
+    this.rightIcon,
+    this.supportsClipboard = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final appLoc = AppLocalizations.of(context)!;
     return Padding(
       padding: const EdgeInsets.only(top: 18),
       child: Row(
@@ -20,11 +27,16 @@ class TransactionDetailItem extends StatelessWidget {
             leftText,
             style: const TextStyle(fontSize: 15),
           ),
-          if (rightIcon != null) ... [
+          if (rightIcon != null) ...[
             rightIcon!,
-          ] else if (rightText != null)... [
+          ] else if (rightText != null) ...[
             Text(rightText!),
           ],
+          if (supportsClipboard) ...[
+            AlcanciaCopyToClipboard(
+                displayText: "${leftText} ${appLoc.alertCopied.toLowerCase()}",
+                textToCopy: rightText!),
+          ]
         ],
       ),
     );
