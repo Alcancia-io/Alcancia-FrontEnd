1b03980447a0547a809204167b4ab16fdc7cded1
Santiago Quihui <santiagoquihui@hotmail.com>
2023-11-07 19:38:51 -0600

    fix: transaction id copy to clipboard

--- lib/src/shared/components/alcancia_copy_clipboard.dart
+++ lib/src/shared/components/alcancia_copy_clipboard.dart
@@ -2,11 +2,11 @@ import 'package:alcancia/src/shared/components/alcancia_snack_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class AlcanciaCopyToClipboard extends StatelessWidget {
+class AlcanciaCopyToClipboardButton extends StatelessWidget {
   final String textToCopy;
   final String displayText;
 
-  const AlcanciaCopyToClipboard({
+  const AlcanciaCopyToClipboardButton({
     super.key,
     required this.displayText,
     required this.textToCopy,
@@ -19,6 +19,39 @@ class AlcanciaCopyToClipboard extends StatelessWidget {
         Icons.copy,
         size: 20,
         color: Colors.blue,
+      ),
+      onTap: () async {
+        Clipboard.setData(ClipboardData(text: textToCopy));
+        ScaffoldMessenger.of(context).showSnackBar(
+          alcanciaSnackBar(context, displayText),
+        );
+      },
+    );
+  }
+}
+
+class AlcanciaCopyToClipboardText extends StatelessWidget {
+  final String textToCopy;
+  final String displayText;
+  final TextStyle? style;
+
+  const AlcanciaCopyToClipboardText({
+    super.key,
+    required this.displayText,
+    required this.textToCopy,
+    this.style = const TextStyle(
+      fontSize: 15,
+      color: Colors.blueAccent,
+      decoration: TextDecoration.underline,
+    ),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Text(
+        textToCopy,
+        style: style,
       ),
       onTap: () async {
         Clipboard.setData(ClipboardData(text: textToCopy));
