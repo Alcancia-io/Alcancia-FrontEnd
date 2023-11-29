ba1b1e90024b7d31f7092946e23297fdcafab842
AlbertoMorillo <amorilloavila25@hotmail.com>
2023-11-28 23:22:42 -0400

    Forgot passwordd and delete account with cognito

--- lib/src/shared/services/auth_service.dart
+++ lib/src/shared/services/auth_service.dart
@@ -9,19 +9,12 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 
 enum AuthChallengeType { MFA_SETUP, SMS_MFA, SOFTWARE_TOKEN_MFA }
 
-class CompletePasswordInput {
-  CompletePasswordInput({this.email, this.newPassword, this.verificationCode});
+class CompleteForgotPasswordInput {
+  CompleteForgotPasswordInput(
+      {this.email, this.newPassword, this.verificationCode});
   String? email = "email";
   String? newPassword = "newPassword";
   String? verificationCode = "123";
-
-  Map<String, dynamic> toMap() {
-    return {
-      "email": email?.toLowerCase(),
-      "newPassword": newPassword,
-      "verificationCode": verificationCode,
-    };
-  }
 }
 
 class AuthService {
@@ -115,22 +108,30 @@ class AuthService {
 
   Future<QueryResult> forgotPassword(String email) async {
     var clientResponse = await client;
-    return clientResponse.query(
-      QueryOptions(
+    return clientResponse.mutate(
+      MutationOptions(
         document: gql(forgotPasswordQuery),
-        variables: {"email": email.toLowerCase()},
+        variables: {
+          "input": {"email": email.toLowerCase()}
+        },
         fetchPolicy: FetchPolicy.noCache,
       ),
     );
   }
 
   Future<QueryResult> completeForgotPassword(
-      CompletePasswordInput queryVariables) async {
+      CompleteForgotPasswordInput queryVariables) async {
     var clientResponse = await client;
     return clientResponse.mutate(
       MutationOptions(
         document: gql(completeForgotPasswordMutation),
-        variables: queryVariables.toMap(),
+        variables: {
+          "input": {
+            "code": queryVariables.verificationCode,
+            "email": queryVariables.email?.toLowerCase(),
+            "newPassword": queryVariables.newPassword,
+          }
+        },
       ),
     );
   }
