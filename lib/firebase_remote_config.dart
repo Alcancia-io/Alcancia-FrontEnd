c2bcf6747b5b644594103691820ac4d821c215dc
AlbertoMorillo <amorilloavila25@hotmail.com>
2024-01-24 11:33:40 -0400

    firebase remote config

---
+++ lib/firebase_remote_config.dart
@@ -0,0 +1,29 @@
+import 'dart:developer';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FirebaseRemoteConfigService {
+  const FirebaseRemoteConfigService({
+    required this.firebaseRemoteConfig,
+  });
+  final FirebaseRemoteConfig firebaseRemoteConfig;
+
+  Future<void> init() async {
+    try {
+      await firebaseRemoteConfig.ensureInitialized();
+      await firebaseRemoteConfig.setConfigSettings(RemoteConfigSettings(
+          fetchTimeout: const Duration(seconds: 10),
+          minimumFetchInterval: Duration.zero));
+    } on FirebaseException catch (e, s) {
+      log("Unable to init remote config", error: e, stackTrace: s);
+    }
+  }
+
+  bool getLiveBool() => firebaseRemoteConfig.getBool("test_bool");
+}
+
+final firebaseRemoteConfigServiceProvider = Provider((ref) =>
+    FirebaseRemoteConfigService(
+        firebaseRemoteConfig: FirebaseRemoteConfig.instance));
