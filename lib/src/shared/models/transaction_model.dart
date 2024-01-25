20a8652929ee6c23c64a544cd0dbe20b5c64b285
AlbertoMorillo <amorilloavila25@hotmail.com>
2024-01-24 21:17:42 -0400

    fix icon withdraw

--- lib/src/shared/models/transaction_model.dart
+++ lib/src/shared/models/transaction_model.dart
@@ -38,20 +38,35 @@ class Transaction {
     final sourceAsset = json["sourceAsset"];
     final createdAt = DateTime.parse(json["createdAt"]);
     final method = jsonDecode(json["method"] ?? "null");
-    final String? concept = method == null || method is String ? null : method["concepto"];
+    final String? concept =
+        method == null || method is String ? null : method["concepto"];
     return Transaction(
-        transactionID: json["id"] as String,
-        createdAt: createdAt,
-        sourceAmount: double.tryParse(json["sourceAmount"].toString()),
-        sourceAsset:
-            CurrencyAsset.values.firstWhereOrNull((e) => e.actualAsset.toLowerCase() == sourceAsset.toString().toLowerCase())?.shownAsset.toUpperCase() ?? sourceAsset.toString().toUpperCase(),
-        targetAsset:
-            CurrencyAsset.values.firstWhereOrNull((e) => e.actualAsset.toLowerCase() == targetAsset.toString().toLowerCase())?.shownAsset.toUpperCase() ?? targetAsset.toString().toUpperCase(),
-        amount: double.parse(json["amount"].toString()),
-        type: TransactionType.values.firstWhere((e) => e.name.toUpperCase() == json["type"], orElse: () => TransactionType.unknown),
-        status: TransactionStatus.values.firstWhere((e) => e.name.toUpperCase() == json["status"], orElse: () => TransactionStatus.unknown),
-        senderId: json["senderId"],
-        receiverId: json["receiverId"],
+      transactionID: json["id"] as String,
+      createdAt: createdAt,
+      sourceAmount: double.tryParse(json["sourceAmount"].toString()),
+      sourceAsset: CurrencyAsset.values
+              .firstWhereOrNull((e) =>
+                  e.actualAsset.toLowerCase() ==
+                  sourceAsset.toString().toLowerCase())
+              ?.shownAsset
+              .toUpperCase() ??
+          sourceAsset.toString().toUpperCase(),
+      targetAsset: CurrencyAsset.values
+              .firstWhereOrNull((e) =>
+                  e.actualAsset.toLowerCase() ==
+                  targetAsset.toString().toLowerCase())
+              ?.shownAsset
+              .toUpperCase() ??
+          targetAsset.toString().toUpperCase(),
+      amount: double.parse(json["amount"].toString()),
+      type: TransactionType.values.firstWhere(
+          (e) => e.name.toUpperCase() == json["type"],
+          orElse: () => TransactionType.unknown),
+      status: TransactionStatus.values.firstWhere(
+          (e) => e.name.toUpperCase() == json["status"],
+          orElse: () => TransactionStatus.unknown),
+      senderId: json["senderId"],
+      receiverId: json["receiverId"],
       concept: concept,
     );
   }
@@ -59,14 +74,38 @@ class Transaction {
 
 extension StatusIcon on Transaction {
   Widget iconForTxnStatus(String currentUserId) {
-    if (status == TransactionStatus.pending) return SvgPicture.asset("lib/src/resources/images/pending.svg", width: 24,);
+    if (status == TransactionStatus.pending)
+      return SvgPicture.asset(
+        "lib/src/resources/images/pending.svg",
+        width: 24,
+      );
     if (status == TransactionStatus.completed) {
-      if (type == TransactionType.deposit || receiverId == currentUserId) return SvgPicture.asset("lib/src/resources/images/deposit.svg", width: 24,);
-      if (type == TransactionType.withdraw || senderId == currentUserId) return SvgPicture.asset("lib/src/resources/images/withdrawal.svg", width: 24,);
+      if (type == TransactionType.deposit || receiverId == currentUserId)
+        return SvgPicture.asset(
+          "lib/src/resources/images/deposit.svg",
+          width: 24,
+        );
+      if (type == TransactionType.withdraw ||
+          type == TransactionType.crypto_withdrawal ||
+          senderId == currentUserId)
+        return SvgPicture.asset(
+          "lib/src/resources/images/withdrawal.svg",
+          width: 24,
+        );
     }
     // FAILED, EXPIRED OR UNKNOWN
-    return SvgPicture.asset("lib/src/resources/images/failed.svg", width: 24,);
+    return SvgPicture.asset(
+      "lib/src/resources/images/failed.svg",
+      width: 24,
+    );
   }
 }
 
-enum TransactionStatus { pending, completed, failed, expired, cancelled, unknown }
+enum TransactionStatus {
+  pending,
+  completed,
+  failed,
+  expired,
+  cancelled,
+  unknown
+}
