ceb116c47059505ff5f3683bcaad6aeceed95898
Silvio Arzeno <silvio@alcancia.io>
2024-02-08 13:11:43 -0400

    Chore: rebase to stage

--- lib/src/shared/models/remote_config_data.dart
+++ lib/src/shared/models/remote_config_data.dart
@@ -1,39 +1,45 @@
 import 'dart:convert';
 
 class RemoteConfigData {
-  final bool minimumAppVersion;
-  final DataObject doConfig;
+  final Map<String, CountryConfig> countryConfig;
 
   RemoteConfigData({
-    required this.minimumAppVersion,
-    required this.doConfig,
+    required this.countryConfig,
   });
 
   factory RemoteConfigData.fromJson(Map<String, dynamic> json) {
+    final Map<String, dynamic> countryConfigJson = json;
+    final Map<String, CountryConfig> countryConfig = countryConfigJson.map(
+      (key, value) => MapEntry(key, CountryConfig.fromJson(value)),
+    );
+
     return RemoteConfigData(
-      minimumAppVersion: json['MinimumAppVersion'] ?? false,
-      doConfig: DataObject.fromJson(json['DO'] ?? {}),
+      countryConfig: countryConfig.cast<String, CountryConfig>(),
     );
   }
 }
 
-class DataObject {
+class CountryConfig {
+  final String icon;
   final bool enabled;
   final Map<String, Currency> currencies;
 
-  DataObject({
+  CountryConfig({
+    required this.icon,
     required this.enabled,
     required this.currencies,
   });
 
-  factory DataObject.fromJson(Map<String, dynamic> json) {
-    final currencies = json['Currencies'] ?? {};
-    final currencyMap =
-        currencies.map((key, value) => MapEntry(key, Currency.fromJson(value)));
+  factory CountryConfig.fromJson(Map<String, dynamic> json) {
+    final Map<String, dynamic> currenciesJson = json['Currencies'];
+    final Map<String, Currency> currencies = currenciesJson.map(
+      (key, value) => MapEntry(key, Currency.fromJson(value)),
+    );
 
-    return DataObject(
-      enabled: json['enabled'] ?? false,
-      currencies: currencyMap.cast<String, Currency>(),
+    return CountryConfig(
+      icon: json['icon'],
+      enabled: json['enabled'],
+      currencies: currencies.cast<String, Currency>(),
     );
   }
 }
@@ -44,7 +50,7 @@ class Currency {
   final String icon;
   final int minAmount;
   final int maxAmount;
-  final Map<String, Bank> bank;
+  final Map<String, Bank> banks;
 
   Currency({
     required this.enabled,
@@ -52,21 +58,22 @@ class Currency {
     required this.icon,
     required this.minAmount,
     required this.maxAmount,
-    required this.bank,
+    required this.banks,
   });
 
   factory Currency.fromJson(Map<String, dynamic> json) {
-    final banks = json['Banks'] ?? {};
-    final bankMap =
-        banks.map((key, value) => MapEntry(key, Bank.fromJson(value)));
+    final Map<String, dynamic> banksJson = json['Banks'];
+    final Map<String, Bank> banks = banksJson.map(
+      (key, value) => MapEntry(key, Bank.fromJson(value)),
+    );
 
     return Currency(
-      enabled: json['enabled'] ?? false,
-      currencyCode: json['currencyCode'] ?? '',
-      icon: json['icon'] ?? '',
-      minAmount: json['minAmount'] ?? 0,
-      maxAmount: json['maxAmount'] ?? 0,
-      bank: bankMap.cast<String, Bank>(),
+      enabled: json['enabled'],
+      currencyCode: json['currencyCode'],
+      icon: json['icon'],
+      minAmount: json['minAmount'],
+      maxAmount: json['maxAmount'],
+      banks: banks.cast<String, Bank>(),
     );
   }
 }
@@ -86,10 +93,10 @@ class Bank {
 
   factory Bank.fromJson(Map<String, dynamic> json) {
     return Bank(
-      enabled: json['enabled'] ?? false,
-      info1: json['info1'] ?? '',
-      info2: json['info2'] ?? '',
-      info3: json['info3'] ?? '',
+      enabled: json['enabled'],
+      info1: json['info1'],
+      info2: json['info2'],
+      info3: json['info3'],
     );
   }
 }
